9e7589da782cbcd138565d7c0a4714108131ee20
Rachis Vsevolod <seva-ra4is@mail.ru>
2025-09-26 14:34:36 +0700

    rank progress fix

--- core/views/game/ranks.py
+++ core/views/game/ranks.py
@@ -8,3 +8,10 @@ class RanksView(ListView):
 
     def get_queryset(self):
         return Rank.objects.prefetch_related("missions", "competence_level__competence").all()
+
+    def get_context_data(self, **kwargs):
+        rank = Rank.objects.get(id=self.request.user.rank)
+        return super().get_context_data(**kwargs) | {
+            "rank": rank,
+            "next_rank": Rank.objects.filter(id=self.request.user.rank + 1).first() or rank,
+        }
